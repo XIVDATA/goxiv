f11a95eb5482d59d13689a8e913cd46fe3e83387
Weneg <nilspreuschoff@googlemail.com>
2022-01-23 00:30:51 +0100

    free company function added to main package

--- goxiv.go
+++ goxiv.go
@@ -3,8 +3,13 @@ package goxiv
 import (
 	"github.com/xivdata/goxiv/controller"
 	"github.com/xivdata/goxiv/model/character"
+	"github.com/xivdata/goxiv/model/freecompany"
 )
 
 func ScrapeCharacter(id int64) character.Character {
 	return controller.ScrapeCharacter(id)
+}
+
+func ScrapeFreecompany(id uint64) freecompany.FreeCompany {
+	return controller.ScrapeFreecompany(id)
 }
