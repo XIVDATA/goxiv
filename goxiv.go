6c06befbe24c77764a05f17c39b6aac6789aa36c
Weneg <nilspreuschoff@googlemail.com>
2022-01-23 22:50:51 +0100

    Modified GoXIV Struct

--- goxiv.go
+++ goxiv.go
@@ -1,28 +1,7 @@
 package goxiv
 
-import (
-	"github.com/xivdata/goxiv/controller"
-	"github.com/xivdata/goxiv/model/character"
-	"github.com/xivdata/goxiv/model/freecompany"
-	"github.com/xivdata/goxiv/model/linkshell"
-	"github.com/xivdata/goxiv/model/pvpteam"
-)
+import "github.com/xivdata/goxiv/controller"
 
-func ScrapeCharacter(id int64) character.Character {
-	return controller.ScrapeCharacter(id)
-}
-
-func ScrapeFreecompany(id uint64) freecompany.FreeCompany {
-	return controller.ScrapeFreecompany(id)
-}
-
-func ScrapePvPTeam(id string) pvpteam.PvPTeam {
-	return controller.ScrapePvPTeam(id)
-}
-
-func ScrapeLinkshell(id string) linkshell.Linkshell {
-	return controller.ScrapeLinkshell(id, false)
-}
-func ScrapeWorldLinkshell(id string) linkshell.Linkshell {
-	return controller.ScrapeLinkshell(id, true)
+type GoXIV struct {
+	controller.Controller
 }
